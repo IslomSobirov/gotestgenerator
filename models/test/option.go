079158689842edb23f2915d4480b1856f0122b28
IslomSobirov <atomm262@gmail.com>
2020-12-16 20:24:50 +0500

    initial commit

---
+++ models/test/option.go
@@ -0,0 +1,28 @@
+package test
+
+//Option of the Question
+type Option struct {
+	id         int
+	optionName string
+	trueOption bool
+	testID     int
+	questionID int
+	createdAt  string
+	updatedAt  string
+}
+
+func createOption() {
+
+}
+
+func updateOption() {
+
+}
+
+func deleteOption(id int) {
+
+}
+
+func all() {
+
+}
---
+++ models/test/question.go
@@ -0,0 +1,10 @@
+package test
+
+//Question for Test
+type Question struct {
+	id           int
+	questionName string
+	testID       int
+	createdAt    string
+	updatedAt    string
+}
