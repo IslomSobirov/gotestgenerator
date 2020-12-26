e8b27b7fcd289e092b6a4e6ab84cb07c7f9b6537
IslomSobirov <atomm262@gmail.com>
2020-12-27 00:03:28 +0500

    .gitignore and env file has been added

--- db/db.go
+++ db/db.go
@@ -2,18 +2,22 @@ package db
 
 import (
 	"database/sql"
+	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-)
-
-const (
-	database = "user_db"
-	user     = "sammy"
-	password = "password"
+	"github.com/joho/godotenv"
 )
 
 //Connect to the db
 func Connect() *sql.DB {
+	enverr := godotenv.Load(".env")
+	if enverr != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	database := os.Getenv("DATABASE")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
 	db, err := sql.Open("mysql", user+":"+password+"@/"+database)
 
 	// if there is an error opening the connection, handle it
